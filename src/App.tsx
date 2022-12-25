8abe8177cab1abd2d5e3ae859fb7d01f77d3dfa2
ypc2e55orj <github@marukunaru.net>
2022-12-25 23:37:43 +0900

    add router, testing query params

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import Logo from './assets/logo.png'
 import './App.css'
+import { useLocation, useSearchParams } from 'react-router-dom';
+import Ayame from './Ayame';
+import { Alert, TopNavigation } from '@cloudscape-design/components';
 
+// クエリパラメータからキーに対応した値を取得する
+// https://reactrouter.com/en/main/hooks/use-search-params
 function App() {
+  const [searchParams] = useSearchParams();
+  const signalingKey = searchParams.get('signalingKey');
+  const roomId = searchParams.get('roomId');
+
   return (
     <div className="App">
+      <TopNavigation
+        identity={{
+          href: `/?signalingKey=${signalingKey}&roomId=${roomId}`,
+          title: "mini-me ui",
+          logo: {
+            src: Logo,
+            alt: "mini-me ui"
+          }
+        }}
+        utilities={[
+          {
+            type: "menu-dropdown",
+            iconName: "settings",
+            ariaLabel: "Settings",
+            title: "Settings",
+            items: [
+              {
+                id: "settings-org",
+                text: "Organizational settings"
+              },
+              {
+                id: "settings-project",
+                text: "Project settings"
+              }
+            ]
+          }
+        ]}
+        i18nStrings={{
+          overflowMenuTriggerText: "More",
+          overflowMenuTitleText: "All"
+        }}
+      />
     </div>
   );
 }
--- src/Ayame.tsx
+++ src/Ayame.tsx
@@ -1,1 +1,41 @@
-impo
+import React, { useEffect } from 'react';
+import AyameWebSdk from '@open-ayame/ayame-web-sdk';
+
+export type Props = {
+    signalingKey: string;
+    roomId: string;
+}
+
+// 参考にしました:
+// OpenAyame/ayame-web-sdkのサンプル
+// https://github.com/OpenAyame/ayame-web-sdk#%E5%8F%8C%E6%96%B9%E5%90%91%E9%80%81%E5%8F%97%E4%BF%A1%E6%8E%A5%E7%B6%9A%E3%81%99%E3%82%8B
+// 非同期通信を用いた描画
+// https://qiita.com/apollo_program/items/01fa3c4621155f64f930
+// videoタグにMediaStreamオブジェクトを渡す
+// https://zenn.dev/k_takahashi23/scraps/38f5d59c37445c
+const Ayame = (props: Props) => {
+    const [streamState, setStreamState] = React.useState(null);
+    useEffect(() => {
+        (async () => {
+            const conn = AyameWebSdk.connection('wss://ayame-labo.shiguredo.app/signaling', props.roomId, Object.assign(AyameWebSdk.defaultOptions, { signalingKey: props.signalingKey }));
+            const mediaStream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
+            await conn.connect(mediaStream);
+            conn.on('disconnect', console.log);
+            conn.on('addstream', (e: any) => {
+                setStreamState(e.stream);
+            });
+        })();
+    }, []);
+
+    return (
+        <div className='Ayame'>
+            <video
+                ref={streamState}
+                autoPlay
+                muted
+                playsInline
+            />
+        </div>
+    );
+};
+export default Ayame;
